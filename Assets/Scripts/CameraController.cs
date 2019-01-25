5d4326ce11a6016e59ca1cc72a62ac6e913f84b1
Eradbar <eradbarram@gmail.com>
2019-01-24 19:21:52 -0800

    Updated assets for game.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,21 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
+public class CameraController : MonoBehaviour {
 
-    public GameObject player;
+	public GameObject player;
 
-    private Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
-    {
-        offset = transform.position - player.transform.position;
-    }
-
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        transform.position = player.transform.position + offset;
-    }
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
 }
