168b74269819576ee0b770df25f4c9b516eda819
Eradbar <eradbarram@gmail.com>
2019-01-28 17:04:18 -0800

    Updated the plasma projectiles.

---
+++ Assets/DeleteMeTimer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteMeTimer : MonoBehaviour {
+	private float timer = 0; 
+	
+	void Update () {
+		timer += Time.deltaTime;
+		if(timer >= .5f){
+			Destroy(gameObject);
+			timer = 0;
+		}
+	}
+}
