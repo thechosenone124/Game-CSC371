41d8dbe4ed215c051c8f07f49d895a9b7bc696fe
djohnson58 <djohnson8785@gmail.com>
2019-01-31 06:39:16 -0800

    Added Weapon Test Scene

---
+++ Assets/Scripts/Gun02Control.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun02Control : MonoBehaviour
+{
+
+   private Animator gunAnim;
+
+   void Start()
+   {
+      gunAnim = GetComponent<Animator>();
+   }
+
+   void Update()
+   {
+
+      Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+
+      if (Input.GetButtonDown("Fire2"))
+      {
+         gunAnim.SetTrigger("Fire");
+      }
+   }
+}
---
+++ Assets/Scripts/MissileMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileMovement : MonoBehaviour {
+
+   public float spawnLength = 0.5f;
+   public float speed = 10f;
+   public Rigidbody2D rb;
+
+   private float timer = 0;
+
+   void Start()
+   {
+      transform.eulerAngles = transform.eulerAngles + new Vector3(0, 0, Random.Range(-30f, 30f));
+      rb.velocity = transform.up * speed;
+   }
+
+   private void FixedUpdate()
+   {
+      timer += Time.deltaTime;
+
+      if (timer >= 0.15f)
+      {
+         transform.eulerAngles = transform.eulerAngles + new Vector3(0, 0, Random.Range(-15f, 15f));
+         rb.velocity = transform.up * speed;
+      }
+
+      if (timer >= spawnLength)
+      {
+         Destroy(gameObject);
+         timer = 0;
+      }
+   }
+}
