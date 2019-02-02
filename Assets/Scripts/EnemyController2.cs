ef3d0fda74df9f31fe7f9e64fdad6f4181c09843
Brandon <bryan04@calpoly.edu>
2019-02-01 16:16:10 -0800

    Copied to enemycontroller2

---
+++ Assets/Scripts/EnemyController2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject target;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Projectile"))
+        {
+            gameObject.SetActive(false);
+            col.gameObject.SetActive(false);
+            GameCtrl.instance.playerScored();
+        }
+
+    }
+}
