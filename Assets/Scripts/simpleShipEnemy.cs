ee7329412a442a2298b6f923a49e638b8c61aa63
erised11 <bryan04@calpoly.edu>
2019-02-06 22:15:38 -0800

    Added small basic ship.
    Will only come after you if you come into its visible sight range. Once you do, it will approach you until it reaches its shooting range. Then it will sit and shoot at you until you move or kill it. (need to make it so it shoots at target)

---
+++ Assets/Scripts/simpleShipEnemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleShipEnemy : MonoBehaviour {
+
+    public GameObject target;
+    public GameObject projectile;
+    public float speed;
+    public float shootRange;
+    public float triggerRange;
+    public float bulletFreq;
+
+    private float timer = 0;
+    private 
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        print(CheckDistance());
+
+        float step = speed * Time.deltaTime;
+        if(CheckDistance() < triggerRange && CheckDistance() > shootRange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+        }
+
+        if (timer >= bulletFreq && CheckDistance() <= shootRange + .5)
+        {
+            GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            timer = 0;
+        }
+
+    }
+
+    private float CheckDistance()
+    {
+        float dist = Vector2.Distance(target.transform.position, transform.position);
+        return dist;
+    }
+}
