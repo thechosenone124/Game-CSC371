ab81582de06432c6865bb861b0576c70f3043755
Brandon <bryan04@calpoly.edu>
2019-02-07 11:56:14 -0800

    Added big ship

---
+++ Assets/Scripts/CarrierShipEnemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrierShipEnemy : MonoBehaviour {
+
+    public GameObject target;
+    public GameObject projectile;
+    public float speed;
+    public float shootRange;
+    public float triggerRange;
+    public float bulletFreq;
+    public float rotateSpeed;
+    public float radius;
+
+    private float angle;
+    private float timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        float step = speed * Time.deltaTime;
+        if (CheckDistance() < triggerRange && CheckDistance() > shootRange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+        }
+
+
+        if (CheckDistance() <= shootRange + .5)
+        {
+            CircleAround();
+            if(timer >= bulletFreq)
+            {
+                Shoot();
+                timer = 0;
+            }
+        }
+
+    }
+
+    private float CheckDistance()
+    {
+        float dist = Vector2.Distance(target.transform.position, transform.position);
+        return dist;
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        bullet.transform.rotation = Quaternion.LookRotation(Vector3.forward, target.transform.position - transform.position);
+
+    }
+
+    private void CircleAround()
+    {
+        angle += rotateSpeed * Time.deltaTime;
+
+        Vector3 offset = new Vector3(Mathf.Sin(angle), Mathf.Cos(angle)) * radius;
+
+        transform.position = target.transform.position + offset;
+
+    }
+
+}
+
---
+++ Assets/Scripts/FaceSideways.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceSideways : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject target;
+    
+    // Update is called once per frame
+    void Update () {
+        Vector3 vectorToTarget = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f,0f,angle);
+    }
+}
