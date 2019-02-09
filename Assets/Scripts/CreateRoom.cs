f591fc3871c4f34a54b27237ffb778d479f5b3ee
Eradbar <eradbarram@gmail.com>
2019-02-09 13:31:21 -0800

    Finished up modularity stuff.

--- Assets/Scripts/CreateRoom.cs
+++ Assets/Scripts/CreateRoom.cs
@@ -3,42 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CreateRoom : MonoBehaviour {
-
-	// Use this for initialization
-	public GameObject obj1;
-	public GameObject obj2;
+	const int COCKPIT = 0;
+	const int WEAPONSROOM = 1;
+	const int ENGINEROOM = 2;
+	const int GUN = 3;
+	const int FOURWAYROOM = 4;
+	private int x,y;
+	private int childNum;
 	private int flag = 0;
-
+	private int moduleType = -1;
 	private int hasChild = 0;
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public GameObject child;
+
+	private int switchCount = 0;
+	void OnMouseDown(){
+		this.transform.parent.GetComponent<SpawnStartingShip>().RoomClicked(x,y,childNum);
 	}
 
-	void OnMouseOver(){
-		if(Input.GetMouseButtonDown(0) && flag == 0){
-			BuildRoom(obj1);
-			flag = 1;
-    	}
-		else if(Input.GetMouseButtonDown(0) && flag == 1){
-			BuildRoom(obj2);
-			flag = 2;
-		}
-		else if(Input.GetMouseButtonDown(0) && flag == 2){
-			flag = 0;
-		}
-	}
-
-	public void BuildRoom(GameObject module){
+	public void BuildRoom(GameObject module, int neighbors, int moduleType){
 		if(hasChild == 1){
 			Destroy(transform.GetChild(0).gameObject);
 			hasChild = 0;
+			if(switchCount == 0){
+				switchCount = 1;
+			}
 		}
 		Instantiate(module,transform.position,Quaternion.identity,transform);
+		this.child = transform.GetChild(switchCount).gameObject;
+		this.moduleType = moduleType;
+		ModuleHandler(moduleType,neighbors);
 		hasChild = 1;
+	}
+
+	public void InitializeRoom(int childNum, int x, int y){
+		this.childNum = childNum;
+		this.x = x;
+		this.y = y;
+	}
+
+	public int GetChildNum(){
+		return childNum;
+	}
+
+	public void UpdateModule(int neighbors){
+		switch(moduleType){
+			case COCKPIT:
+				child.GetComponent<CockpitConfigurer>().Doors(neighbors);
+				break;
+			case WEAPONSROOM:
+				child.GetComponent<WeaponsRoomConfigurer>().Doors(neighbors);
+				break;
+			case ENGINEROOM:
+				child.GetComponent<EngineConfigurer>().Doors(neighbors);
+				break;
+			case GUN:
+				break;
+			case FOURWAYROOM:
+				child.GetComponent<RoomConfigurer>().Doors(neighbors);
+				break;
+		}
+	}
+
+	public int GetModuleType(){
+		return moduleType;
+	}
+
+	private void ModuleHandler(int moduleType, int neighbors){
+		switch(moduleType){
+			case COCKPIT:
+				child.GetComponent<CockpitConfigurer>().Doors(neighbors);
+				break;
+			case WEAPONSROOM:
+				child.GetComponent<WeaponsRoomConfigurer>().Doors(neighbors);
+				break;
+			case ENGINEROOM:
+				child.GetComponent<EngineConfigurer>().Doors(neighbors);
+				break;
+			case GUN:
+				child.gameObject.GetComponent<GunConfigurer>().FaceNeighbor(neighbors);
+				break;
+			case FOURWAYROOM:
+				child.GetComponent<RoomConfigurer>().Doors(neighbors);
+				break;
+		}
 	}
 }
