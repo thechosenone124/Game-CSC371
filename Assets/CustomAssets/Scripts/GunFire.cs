79da4b334a5929c0f8d4e90d11384ca3833261ef
Bryan <thechosenone124@gmail.com>
2019-02-09 23:57:12 -0800

    main menu added, note that buttons dont work until project is built

---
+++ Assets/CustomAssets/Scripts/GunFire.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFire : MonoBehaviour
+{
+   public int numProjectiles = 1;
+   public GameObject projectile;
+
+   void Fire()
+   {
+      for (int i = 0; i < numProjectiles; i++)
+      {
+         Instantiate(projectile, transform.position, transform.rotation);
+      }
+   }
+}
---
+++ Assets/CustomAssets/Scripts/MissileMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileMovement : MonoBehaviour {
+
+   public float spawnLength = 0.5f;
+   public float speed = 10f;
+   public float timeToFlurry = 0.25f;
+   public float timeBetweenTurns = 0.5f;
+   public float turnAngleRange = 30f;
+   public Rigidbody2D rb;
+
+   private float timer = 0;
+   private float timeSinceLastTurn = 0;
+
+   void Start()
+   {
+      transform.eulerAngles = transform.eulerAngles + new Vector3(0, 0, Random.Range(-10f, 10f));
+      rb.velocity = transform.up * speed;
+   }
+
+   private void FixedUpdate()
+   {
+      timer += Time.deltaTime;
+
+      if (timer >= timeToFlurry && timeSinceLastTurn >= timeBetweenTurns)
+      {
+         transform.eulerAngles = transform.eulerAngles + new Vector3(0, 0, Random.Range(-turnAngleRange, turnAngleRange));
+         rb.velocity = transform.up * speed;
+         timeSinceLastTurn = 0;
+      }
+      else
+      {
+         timeSinceLastTurn+= Time.deltaTime;
+      }
+
+      if (timer >= spawnLength)
+      {
+         Destroy(gameObject);
+         timer = 0;
+      }
+   }
+}
