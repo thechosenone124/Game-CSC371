d402e3f973537c319fc544e6b75ba954271d75f6
djohnson58 <djohnson8785@gmail.com>
2019-02-11 18:53:33 -0800

    Restored Ship Transform Movement

--- Assets/Scripts/ShipMovementController.cs
+++ Assets/Scripts/ShipMovementController.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class ShipMovementController : MonoBehaviour {
 
-   public Rigidbody2D shipRB;
-   public float speed = 0.5f;
+    public float speed = 0.5f;
 
-   private Vector2 input;
+    private Vector2 input;
 
-   // Use this for initialization
-   void Start () {
-      shipRB = GetComponent<Rigidbody2D>();
+    // Use this for initialization
+    void Start () {
+		
 	}
 	
 	// Update is called once per frame
@@ -19,20 +18,11 @@ public class ShipMovementController : MonoBehaviour {
 		
 	}
 
-   public void MoveShip(PlayerInputContainer pic)
-   {
-      if (pic.isOperatingStation)
-      {
-         shipRB.AddForce(new Vector2(pic.GetHorizontal(), pic.GetVertical()) * speed);
-         /*if (pic.GetHorizontal() == 0 && pic.GetVertical() == 0)
-         {
-            shipRB.velocity = Vector3.zero;
-         }
-         else
-         {
-            shipRB.AddForce(new Vector2(pic.GetHorizontal(), pic.GetVertical()) * speed);
-            //transform.position += new Vector3(pic.GetHorizontal(), pic.GetVertical(), 0f) * speed;
-         }*/
-      }
-   }
+    public void MoveShip(PlayerInputContainer pic)
+    {
+        if (pic.isOperatingStation)
+        {
+            transform.position += new Vector3(pic.GetHorizontal(), pic.GetVertical(), 0f) * speed;
+        }
+    }
 }
