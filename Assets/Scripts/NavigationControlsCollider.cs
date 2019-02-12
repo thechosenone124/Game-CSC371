d4b0be66b94f740fd9a7e1e0e13c8e80f9a40762
Eradbar <eradbarram@gmail.com>
2019-02-11 21:39:22 -0800

    Backdated Demo scene to Davin's version.

--- Assets/Scripts/NavigationControlsCollider.cs
+++ Assets/Scripts/NavigationControlsCollider.cs
@@ -25,7 +25,8 @@ public class NavigationControlsCollider : MonoBehaviour {
                     Debug.Log("Name on pilot collision: " + collision.gameObject.name);
                     Debug.Log(collision.gameObject.name + "'s a button: " + pcon.GetAButton());
                     pcon.isOperatingStation = true;
-                    collision.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+                    //collision.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+                    collision.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
                 }
             }
             else if (pcon.GetBButton() && pcon.isOperatingStation)
@@ -34,7 +35,8 @@ public class NavigationControlsCollider : MonoBehaviour {
                 if (shipInfo.RemovePilot(collision.gameObject)) // returns true if collision.gameObject is the player set as pilot
                 {
                     pcon.isOperatingStation = false;
-                    collision.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                    //collision.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                    collision.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
                 }
 
             }
