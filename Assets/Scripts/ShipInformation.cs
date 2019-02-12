5f18908095c7a9b1d31e6440a69249d7f2d35773
Brandon <bryan04@calpoly.edu>
2019-02-12 13:42:07 -0800

    Update ShipInformation.cs

--- Assets/Scripts/ShipInformation.cs
+++ Assets/Scripts/ShipInformation.cs
@@ -1,23 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShipInformation : MonoBehaviour {
 
     public float maxHealth = 100f;
-    
+    public Image currentHealthBar;
+    public Text ratioText;
+
+    private float currentHealth;
     private ShipMovementController moveCon;
     private ShipWeaponController weapCon;
     private GameObject pilot;
     private GameObject gunner;
 	// Use this for initialization
 	void Start () {
+        currentHealth = maxHealth;
         moveCon = GetComponent<ShipMovementController>();
         weapCon = GetComponent<ShipWeaponController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        //Health
+        UpdateHealthBar();
 
         if (pilot  != null) moveCon.MoveShip(pilot.GetComponent<PlayerInputContainer>());
         if (gunner != null)
@@ -68,6 +76,25 @@ public class ShipInformation : MonoBehaviour {
             return true;
         }
         return false;
+    }
+
+    //Health manipulation
+    private void UpdateHealthBar()
+    {
+        float ratio = currentHealth / maxHealth;
+        currentHealthBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        ratioText.text = (ratio * 100).ToString("0") + "%";
+    }
+
+    private void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            GameCtrl.instance.playerDied();
+        }
+        UpdateHealthBar();
     }
 
 
