e54abeeabc2ecbc6c6a7c0f029567ed84a91b129
Bryan <thechosenone124@gmail.com>
2019-02-12 13:29:02 -0800

    resolving final conflicts

--- Assets/LoadSceneOnClick.cs
+++ Assets/LoadSceneOnClick.cs
@@ -7,11 +7,9 @@ public class LoadSceneOnClick : MonoBehaviour
     private AssetBundle myLoadedAssetBundle;
     public void Start()
     {
-        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/CustomAssets/Scenes");
     }
     public void LoadByName(string sceneName)
     {
-        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/CustomAssets/Scenes");
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,43 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class PlayerController : MonoBehaviour{
+public class PlayerController : MonoBehaviour
+{
 
     public float speed;
     public float maxSpeed = 10f;
     public float drag = 5f;
     private Rigidbody2D rb2d;
     public GameObject projectile;
-    private bool shoot;
-    private int count;
+    private bool canShoot;
+    private int score;
     public bool navigationControls;
     public bool gunControls;
 
-    void Start(){
+    void Start()
+    {
         score = 0;
         rb2d = GetComponent<Rigidbody2D>();
-        shoot = false;
-        navigationControls = false;
-        gunControls = false;
+        rb2d.drag = drag;
+        canShoot = false;
     }
 
-    void FixedUpdate(){
-        if (navigationControls)
+    void FixedUpdate()
+    {
+        float moveHori = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+        Debug.Log(moveHori);
+        Debug.Log(moveVert);
+        Vector2 move = new Vector2(moveHori, moveVert);
+
+        if ((rb2d.velocity + move).magnitude < maxSpeed)
         {
-           float moveHori = Input.GetAxis("Horizontal");
-           float moveVert = Input.GetAxis("Vertical");
-           Vector2 move = new Vector2 (moveHori,moveVert);
-           rb2d.AddForce(move *speed);
+            rb2d.AddForce(move * speed);
         }
     }
 
-    void Update() {
-       if (Input.GetKeyDown(KeyCode.LeftShift) && shoot && gunControls)
-         {
-             GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
-         }
+    void Update()
+    {
+        if (navigationControls)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift) && canShoot && gunControls)
+            {
+                GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            }
+        }
     }
 
+    /*
     void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("PlayerController -- Hit something");
@@ -54,7 +64,8 @@ public class PlayerController : MonoBehaviour{
             //collision.gameObject.SetActive(false); 
         }
     }
-    
+    */
+
     public void disableMovement()
     {
         rb2d.velocity = Vector2.zero;
