16aa0ef21a998ddea8245a253bb0b73e242ab601
Eradbar <eradbarram@gmail.com>
2019-02-13 12:56:02 -0800

    Added end game states.

---
+++ Assets/Scripts/ShipDestroyed.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipDestroyed : MonoBehaviour {
+	void Update () {
+		if(GameController.instance.getCurrentHealth() <= 0){
+			PlayerDied();
+		}
+	}
+	void PlayerDied(){
+		gameObject.SetActive(false);
+		GameController.instance.PlayerLoses();	
+	}
+}
