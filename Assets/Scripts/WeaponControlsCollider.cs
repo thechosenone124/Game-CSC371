d0f7f18806b223cc47a3b664c570cd2600ae0ff8
djohnson58 <djohnson8785@gmail.com>
2019-02-12 22:55:50 -0800

    Finishing Demo Level

--- Assets/Scripts/WeaponControlsCollider.cs
+++ Assets/Scripts/WeaponControlsCollider.cs
@@ -28,7 +28,7 @@ public class WeaponControlsCollider : MonoBehaviour {
                     Debug.Log(collision.gameObject.name + "'s a button: " + pcon.GetAButton());
                     pcon.isOperatingStation = true;
                     collision.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
-                    //collision.gameObject.GetComponent<CameraHolder>().playerCamera.SetActive(false);
+                    collision.gameObject.GetComponent<CameraHolder>().playerCamera.SetActive(false);
             }
             }
             
@@ -39,7 +39,7 @@ public class WeaponControlsCollider : MonoBehaviour {
                 {
                     pcon.isOperatingStation = false;
                     collision.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
-                    //collision.gameObject.GetComponent<CameraHolder>().playerCamera.SetActive(true);
+                    collision.gameObject.GetComponent<CameraHolder>().playerCamera.SetActive(true);
                 }
 
             }
