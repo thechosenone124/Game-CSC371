224e8b9ba90e32bfa8d2085dc25cd7c28ac48ce3
erised11 <bryan04@calpoly.edu>
2019-03-03 23:25:23 -0800

    Layered boss script

---
+++ Assets/LayeredBossScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LayeredBossScript : MonoBehaviour {
+
+    public Sprite[] layers;
+
+    private SpriteRenderer sr;
+    private int layerIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = layers[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sr.sprite = layers[layerIndex];
+    }
+
+    public void IncreaseLayerIndex()
+    {
+        layerIndex++;
+    }
+}
