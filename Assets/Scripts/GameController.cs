5603346f64c7a2f63b893a03c4a78b06e0ec67e3
Noah Paige <noahlandonpaige@gmail.com>
2019-03-05 14:41:08 -0800

    Reduced spawners, tweaks to bullets

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -144,6 +144,7 @@ public class GameController : MonoBehaviour
 
    public void PlayerLoses()
    {
+        GameObject.Find("Ship").SetActive(false);
       gameStateText.text = "You Died!";
    }
 
