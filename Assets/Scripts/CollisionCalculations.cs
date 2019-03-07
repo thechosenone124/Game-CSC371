f0b7de5d9132adde03ae8291321060674b38f881
Brandon <bryan04@calpoly.edu>
2019-03-07 12:37:08 -0800

    maybe fixed shield regen?

--- Assets/Scripts/CollisionCalculations.cs
+++ Assets/Scripts/CollisionCalculations.cs
@@ -23,10 +23,10 @@ public class CollisionCalculations : MonoBehaviour {
             timeStamp = Time.time + coolDownPeriod;
             Destroy(collision.gameObject);
             GameController.instance.SendMessage("TakeDamage", 3);
-            if(Time.time > timeStamp && !GameController.instance.shieldBroken)
-            {
-                GameController.instance.SendMessage("RegenerateShield");
-            }
+        }
+        if (Time.time > timeStamp && !GameController.instance.shieldBroken)
+        {
+            GameController.instance.SendMessage("RegenerateShield");
         }
     }
 }
