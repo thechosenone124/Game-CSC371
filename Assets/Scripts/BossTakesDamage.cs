e35fb42ea8693b6af4253ea3045b40c564d81752
Brandon <bryan04@calpoly.edu>
2019-03-12 13:41:00 -0700

    Added boss stuff

--- Assets/Scripts/BossTakesDamage.cs
+++ Assets/Scripts/BossTakesDamage.cs
@@ -6,24 +6,35 @@ public class BossTakesDamage : MonoBehaviour
 {
 
    // Use this for initialization
-   private bool gotHit = false;
-   public int enemyHealth = 200;
-   private float damageTime = 0;
-   public float redTime;
+    private bool gotHit = false;
+    public int enemyHealth = 200;
+    public int bossNumber;
+    public int numberOfLayers;
+    private float damageTime = 0;
+    private float damageTaken = 0;
+    public float redTime;
+    private float damageNeededToIncreaseLayer;
+     
    void Start()
    {
-
+        damageNeededToIncreaseLayer = enemyHealth / numberOfLayers;
    }
 
    // Update is called once per frame
    void Update()
    {
-      if (gotHit)
+        if(damageTaken > damageNeededToIncreaseLayer)
+        {
+            SendMessage("IncreaseLayerIndex");
+            damageTaken = 0;
+        }
+        if (gotHit)
       {
-         enemyHealth -= 1;
-         gotHit = false;
-         damageTime += redTime;
-         gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+            enemyHealth -= 1;
+            damageTaken += 1;
+            gotHit = false;
+            damageTime += redTime;
+            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
       }
       if (damageTime > 0)
       {
@@ -36,8 +47,27 @@ public class BossTakesDamage : MonoBehaviour
       }
       if (enemyHealth == 0)
       {
-         GameController.instance.PlayerWins();
-      }
+            if(bossNumber == 0)
+            {
+                GameController.instance.TutorialBossIsDead();
+            }
+            else if(bossNumber == 1)
+            {
+                GameController.instance.Boss1IsDead();
+            }
+            else if(bossNumber == 2)
+            {
+                GameController.instance.Boss2IsDead();
+            }
+            else if (bossNumber == 3)
+            {
+                GameController.instance.Boss3IsDead();
+            }
+            else if (bossNumber == 4)
+            {
+                GameController.instance.Boss4IsDead();
+            }
+        }
    }
    void OnTriggerEnter2D(Collider2D col)
    {
