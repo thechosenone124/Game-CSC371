d4083afa5d9da5940137bc2106388c8ebc2ce484
Eradbar <eradbarram@gmail.com>
2019-03-13 12:45:44 -0700

    3/13/19 Fixed some bugs

--- Assets/Scripts/SpawnStartingShip.cs
+++ Assets/Scripts/SpawnStartingShip.cs
@@ -270,6 +270,7 @@ public class SpawnStartingShip : MonoBehaviour {
 	}
 
 	void BuildShip(){
+		GameObject.Find("Ship").transform.rotation = Quaternion.identity;
 		for(int i = 0; i < 5; i++){
 			for(int j = 0; j < 5; j++){
 				if(shipLayout[j,i] != -1){
