2e4654a0333ec7e80273a8d43076b93546d6463e
Eradbar <eradbarram@gmail.com>
2019-03-15 16:50:32 -0700

    3/15/19 fixed some stuff

--- Assets/Scripts/PickupController.cs
+++ Assets/Scripts/PickupController.cs
@@ -42,7 +42,7 @@ public class PickupController : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //if (collision.transform.root.CompareTag("Cockpit") || collision.transform.root.CompareTag("Gun") || collision.transform.root.CompareTag("Room") || collision.transform.root.CompareTag("EngineRoom"))
-        if(collision.transform.root.CompareTag("PlayerShip"))
+        if(collision.CompareTag("EngineRoom"))
         {
             Debug.Log("PickupController: OnTriggerEnter2D -- " + name + " collided with ship");
             GameController.instance.GetComponent<Inventory>().AddItem(itemType);
